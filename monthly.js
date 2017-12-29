b4483d3898bac65ba86a00bf72078d3648766782
davgur <davgur.ru@gmail.com>
2017-12-29 09:10:36 +0200

    update

--- monthly.js
+++ monthly.js
@@ -2,178 +2,174 @@
 //monthly.run();
 
 function LoadMonthly(_resultCurrentRowId) {
-    var ss = SpreadsheetApp.getActiveSpreadsheet();
-    var resultSheet = ss.getSheetByName('board');
-    var configsSheet = ss.getSheetByName('config');
-    _resultCurrentRowId++;
+  var ss           = SpreadsheetApp.getActiveSpreadsheet();
+  var resultSheet  = ss.getSheetByName('board');
+  var configsSheet = ss.getSheetByName('config');
+  _resultCurrentRowId++;
 
-    var dataValues = SpreadsheetApp.openById("1LlRo5Ob5Bw8penUEakQj_1NyfmmD-T_Dama-k81dohQ").getSheetByName("Sheet1").getRange('A3:AG1000').getValues();
-    dataValues = _filter(dataValues);
+  var dataValues = SpreadsheetApp.openById('1LlRo5Ob5Bw8penUEakQj_1NyfmmD-T_Dama-k81dohQ').getSheetByName('Sheet1').getRange('A2:AG1500').getValues();
+  dataValues     = _filter(dataValues);
 
-    return {
-        run: run
-    };
+  return {
+    run: run
+  };
 
-    function _filter(arr) {
-        var result = {yearsList: []}, data = [];
-        var _now = new Date();
-        arr.filter(function (val, key) {
-            if (val[8] !== 3 || val[2] < _now) {
-                return false;
-            }
-            if (val[8] === 0) {
-                return !!arr[key + 1] && arr[key + 1][8] !== 0;
-            }
-            return !!val[8];
-        }).sort(function (a, b) {
-            if (a[2] < b[2]) {
-                return -1;
-            }
-            if (a[2] > b[2]) {
-                return 1;
-            }
-            if (a[5] < b[5]) {
-                return -1;
-            }
-            if (a[5] > b[5]) {
-                return 1;
-            }
-            return 0;
-        }).forEach(function (val) {
-            var key = new Date(val[2]).getFullYear();
-            if (!result[key]) {
-                result[key] = [];
-                result.yearsList.push(key);
-            }
-            result[key].push(val);
+  function _filter(arr) {
+    var result = { yearsList: [] }, data = [];
+    var _now   = new Date();
+    arr.filter(function (val, key) {
+      return val[5] == 3 && val[0] >= _now;
+    }).sort(function (a, b) {
+      var a2Num = new Number(a[0]);
+      var b2Num = new Number(b[0]);
+      var a5Num = new Number(a[1].split(':')[0]);
+      var b5Num = new Number(b[1].split(':')[0]);
+      if (a2Num < b2Num) {
+        return -1;
+      }
+      if (a2Num > b2Num) {
+        return 1;
+      }
+      if (a5Num < a5Num) {
+        return -1;
+      }
+      if (a5Num > a5Num) {
+        return 1;
+      }
+      return 0;
+    }).forEach(function (val) {
+      var key = new Date(val[0]).getFullYear();
+      if (!result[key]) {
+        result[key] = [];
+        result.yearsList.push(key);
+      }
+      result[key].push(val);
 
-        });
-        return result;
-    }
+    });
+    return result;
+  }
 
-    function run() {
-        dataValues.yearsList.forEach(function (year) {
-            printYear(year, dataValues[year]);
-        });
-        resultSheet.getRange(_resultCurrentRowId + 1, 1, 50, 50).clear();
-    }
+  function run() {
+    dataValues.yearsList.forEach(function (year) {
+      printYear(year, dataValues[year]);
+    });
+    resultSheet.getRange(_resultCurrentRowId + 1, 1, 50, 50).clear();
+  }
 
-    function printYear(year, data) {
-        _printTableTitle(year);
-        var now_month;
-        data.forEach(function (row, i) {
-            _resultCurrentRowId++;
-            var data = rowToObject(row);
-            var _date = new Date(data.date);
-            _date.setHours(_date.getHours() + 8);
-            var temp_month = _date.getMonth();
-            if (now_month !== temp_month) {
-                _formatMonth(temp_month);
-                now_month = temp_month;
-                _printSeparators();
-                _resultCurrentRowId++;
-            }
-            resultSheet.setRowHeight(_resultCurrentRowId, 24);
-            //heb
-            _addContent(data, 2, data.heb, 0);
-            //english
-            _addContent(data, 6, data.eng, 1);
-            //rus
-            _addContent(data, 10, data.rus, 2);
-            //esp
-            _addContent(data, 14, data.esp, 3);
-
-            _printSeparators();
-        });
-        _resultCurrentRowId++;
-        _toBlack(resultSheet.getRange(_resultCurrentRowId, 1, 1, 17));
-    }
-
-
-    function _printTableTitle(year) {
-        _resultCurrentRowId++;
-        var hebText = 'לוח אירועים שנתי ';
-        var rusText = "Annual Board of Events ";
-        var engText = "Расписание событий на ";
-        var espText = "Tabla Anual de Eventos Para ";
-
-        //heb
-        resultSheet.getRange(_resultCurrentRowId, 2, 1, 3).merge().setFontSize(24).setFontWeight("bold").setHorizontalAlignment("center").setValue(hebText + year).setBackground("#6d9eeb");
-        //eng
-        resultSheet.getRange(_resultCurrentRowId, 6, 1, 3).merge().setFontSize(24).setFontWeight("bold").setHorizontalAlignment("center").setValue(engText + year).setBackground("#00ff00");
-        //rus
-        resultSheet.getRange(_resultCurrentRowId, 10, 1, 3).merge().setFontSize(24).setFontWeight("bold").setHorizontalAlignment("center").setValue(rusText + year + " год").setBackground("#ffff00");
-        //esp
-        resultSheet.getRange(_resultCurrentRowId, 14, 1, 3).merge().setFontSize(24).setFontWeight("bold").setHorizontalAlignment("center").setValue(espText + year).setBackground("#e06666");
-        resultSheet.setRowHeight(_resultCurrentRowId, 50);
-
+  function printYear(year, data) {
+    _printTableTitle(year);
+    var now_month;
+    data.forEach(function (row, i) {
+      _resultCurrentRowId++;
+      var data  = rowToObject(row);
+      var _date = new Date(data.date);
+      _date.setHours(_date.getHours() + 8);
+      var temp_month = _date.getMonth();
+      if (now_month !== temp_month) {
+        _formatMonth(temp_month);
+        now_month = temp_month;
         _printSeparators();
-        _toBlack(resultSheet.getRange(_resultCurrentRowId + 1, 1, 1, 17));
         _resultCurrentRowId++;
+      }
+      resultSheet.setRowHeight(_resultCurrentRowId, 24);
+      //heb
+      _addContent(data, 2, data.heb, 0);
+      //english
+      _addContent(data, 6, data.eng, 1);
+      //rus
+      _addContent(data, 10, data.rus, 2);
+      //esp
+      _addContent(data, 14, data.esp, 3);
+
+      _printSeparators();
+    });
+    _resultCurrentRowId++;
+    _toBlack(resultSheet.getRange(_resultCurrentRowId, 1, 1, 17));
+  }
+
+  function _printTableTitle(year) {
+    _resultCurrentRowId++;
+    var hebText = 'לוח אירועים שנתי ';
+    var engText = 'Annual Board of Events ';
+    var rusText = 'Расписание событий на ';
+    var espText = 'Tabla Anual de Eventos Para ';
+
+    //heb
+    resultSheet.getRange(_resultCurrentRowId, 2, 1, 3).merge().setFontSize(24).setFontWeight('bold').setHorizontalAlignment('center').setValue(hebText + year).setBackground('#6d9eeb');
+    //eng
+    resultSheet.getRange(_resultCurrentRowId, 6, 1, 3).merge().setFontSize(24).setFontWeight('bold').setHorizontalAlignment('center').setValue(engText + year).setBackground('#00ff00');
+    //rus
+    resultSheet.getRange(_resultCurrentRowId, 10, 1, 3).merge().setFontSize(24).setFontWeight('bold').setHorizontalAlignment('center').setValue(rusText + year + ' год').setBackground('#ffff00');
+    //esp
+    resultSheet.getRange(_resultCurrentRowId, 14, 1, 3).merge().setFontSize(24).setFontWeight('bold').setHorizontalAlignment('center').setValue(espText + year).setBackground('#e06666');
+    resultSheet.setRowHeight(_resultCurrentRowId, 50);
+
+    _printSeparators();
+    _toBlack(resultSheet.getRange(_resultCurrentRowId + 1, 1, 1, 17));
+    _resultCurrentRowId++;
+  }
+
+  function _formatMonth(monthIndex) {
+    var _bgColors = configsSheet.getRange(3, 11, 4, 1).getValues();
+    var _months   = configsSheet.getRange(3, 1, 12, 4).getValues();
+    resultSheet.setRowHeight(_resultCurrentRowId, 24);
+
+    //heb
+    resultSheet.getRange(_resultCurrentRowId, 2, 1, 3).merge().setFontSize(16).setFontWeight('bold').setHorizontalAlignment('center').setValue(_months[monthIndex][0]).setBackground(_bgColors[0][0]);
+    //rus
+    resultSheet.getRange(_resultCurrentRowId, 6, 1, 3).merge().setFontSize(16).setFontWeight('bold').setHorizontalAlignment('center').setValue(_months[monthIndex][1]).setBackground(_bgColors[1][0]);
+    //eng
+    resultSheet.getRange(_resultCurrentRowId, 10, 1, 3).merge().setFontSize(16).setFontWeight('bold').setHorizontalAlignment('center').setValue(_months[monthIndex][2]).setBackground(_bgColors[2][0]);
+    //esp
+    resultSheet.getRange(_resultCurrentRowId, 14, 1, 3).merge().setFontSize(16).setFontWeight('bold').setHorizontalAlignment('center').setValue(_months[monthIndex][3]).setBackground(_bgColors[3][0]);
+  }
+
+  function _addContent(data, startIndex, value, langIndex) {
+    var _values   = [];
+    var _weekDays = configsSheet.getRange(3, 6, 7, 4).getValues();
+    var range     = resultSheet.getRange(_resultCurrentRowId, startIndex, 1, 3);
+    _clearFormat(range, langIndex === 0);
+    range.setFontSize(12);
+
+    _values[2] = Utilities.formatDate(new Date(data.date), 'Asia/Jerusalem', 'dd/MM/yyyy');
+    _values[1] = _weekDays[data.weekDay][langIndex];
+    _values[0] = value;
+    if (langIndex === 0) {
+      _values.reverse();
+      resultSheet.getRange(_resultCurrentRowId, startIndex, 1, 2).setFontWeight('bold');
+    } else {
+      resultSheet.getRange(_resultCurrentRowId, startIndex + 1, 1, 2).setFontWeight('bold');
     }
 
-    function _formatMonth(monthIndex) {
-        var _bgColors = configsSheet.getRange(3, 11, 4, 1).getValues();
-        var _months = configsSheet.getRange(3, 1, 12, 4).getValues();
-        resultSheet.setRowHeight(_resultCurrentRowId, 24);
+    range.setValues([_values]);
+  }
 
-        //heb
-        resultSheet.getRange(_resultCurrentRowId, 2, 1, 3).merge().setFontSize(16).setFontWeight("bold").setHorizontalAlignment("center").setValue(_months[monthIndex][0]).setBackground(_bgColors[0][0]);
-        //rus
-        resultSheet.getRange(_resultCurrentRowId, 6, 1, 3).merge().setFontSize(16).setFontWeight("bold").setHorizontalAlignment("center").setValue(_months[monthIndex][1]).setBackground(_bgColors[1][0]);
-        //eng
-        resultSheet.getRange(_resultCurrentRowId, 10, 1, 3).merge().setFontSize(16).setFontWeight("bold").setHorizontalAlignment("center").setValue(_months[monthIndex][2]).setBackground(_bgColors[2][0]);
-        //esp
-        resultSheet.getRange(_resultCurrentRowId, 14, 1, 3).merge().setFontSize(16).setFontWeight("bold").setHorizontalAlignment("center").setValue(_months[monthIndex][3]).setBackground(_bgColors[3][0]);
-    }
+  function rowToObject(row) {
+    return {
+      date: row[0],
+      weekDay: row[0].getDay(),
+      heb: row[3],
+      eng: row[8],
+      rus: row[11],
+      esp: row[14]
+    };
+  }
 
-    function _addContent(data, startIndex, value, langIndex) {
-        var _values = [];
-        var _weekDays = configsSheet.getRange(3, 6, 7, 4).getValues();
-        var range = resultSheet.getRange(_resultCurrentRowId, startIndex, 1, 3);
-        _clearFormat(range, langIndex === 0);
-        range.setFontSize(12);
+  function _printSeparators() {
+    _toBlack(resultSheet.getRange(_resultCurrentRowId, 1));
+    _toBlack(resultSheet.getRange(_resultCurrentRowId, 5));
+    _toBlack(resultSheet.getRange(_resultCurrentRowId, 9));
+    _toBlack(resultSheet.getRange(_resultCurrentRowId, 13));
+    _toBlack(resultSheet.getRange(_resultCurrentRowId, 17));
+  }
 
-        _values[2] = Utilities.formatDate(new Date(data.date), "Asia/Jerusalem", "dd/MM/yyyy");
-        _values[1] = _weekDays[data.weekDay][langIndex];
-        _values[0] = value;
-        if (langIndex === 0) {
-            _values.reverse();
-            resultSheet.getRange(_resultCurrentRowId, startIndex - 1, 1, 2).setFontWeight("bold");
-        } else {
-            resultSheet.getRange(_resultCurrentRowId, startIndex + 1, 1, 2).setFontWeight("bold");
-        }
+  function _toBlack(range) {
+    range.clear().setBackground('#cccccc');
+  }
 
-        range.setValues([_values]);
-    }
-
-    function rowToObject(row) {
-        return {
-            date: row[2],
-            weekDay: row[2].getDay(),
-            rus: row[21],
-            heb: row[4],
-            eng: row[13],
-            esp: row[29]
-        };
-    }
-
-
-    function _printSeparators() {
-        _toBlack(resultSheet.getRange(_resultCurrentRowId, 1));
-        _toBlack(resultSheet.getRange(_resultCurrentRowId, 5));
-        _toBlack(resultSheet.getRange(_resultCurrentRowId, 9));
-        _toBlack(resultSheet.getRange(_resultCurrentRowId, 13));
-        _toBlack(resultSheet.getRange(_resultCurrentRowId, 17));
-    }
-
-    function _toBlack(range) {
-        range.clear().setBackground("#cccccc");
-    }
-
-    function _clearFormat(range, isHeb) {
-        range.clearFormat();
-        var dir = isHeb ? "right" : "left";
-        range.setHorizontalAlignment(dir);
-    }
+  function _clearFormat(range, isHeb) {
+    range.clearFormat();
+    var dir = isHeb ? 'right' : 'left';
+    range.setHorizontalAlignment(dir);
+  }
 }
--- weekly.js
+++ weekly.js
@@ -2,215 +2,218 @@
 //weekly.run();
 
 function LoadWeekly(_resultCurrentRowId) {
-    var ss = SpreadsheetApp.getActiveSpreadsheet();
-    var resultSheet = ss.getSheetByName('board');
-    var resultValues = resultSheet.getDataRange().getValues();
-    var _theDay = {};
+  var ss           = SpreadsheetApp.getActiveSpreadsheet();
+  var resultSheet  = ss.getSheetByName('board');
+  var configsSheet = ss.getSheetByName('config');
+  var _theDay      = {};
+  var _weekDays    = configsSheet.getRange(3, 6, 7, 4).getValues();
 
+  var dataValues = SpreadsheetApp.openById('1LlRo5Ob5Bw8penUEakQj_1NyfmmD-T_Dama-k81dohQ').getSheetByName('Sheet1').getRange('A3:AG1000').getValues();
+  dataValues     = _filter(dataValues);
+  return {
+    run: function (runAfterAll) {
+      _separator(resultSheet.getRange(_resultCurrentRowId, 1, 1, 17));
+      _printTableTitle();
+      dataValues.forEach(function (row, key) {
+        if (_theDay.date && row[0] && _theDay.date.date.getTime() != row[0].getTime()) {
+          _printDay(_theDay);
+          _theDay = {};
+        }
+        if (!_theDay.date) {
+          _theDay.date   = _parseDate(row);
+          _theDay.events = [];
+        }
+        _theDay.events.push(_parseEvent(row));
+      });
+      _separator(resultSheet.getRange(_resultCurrentRowId + 1, 1, 1, 17));
+      runAfterAll(_resultCurrentRowId);
+    }
+  };
 
-    var dataValues = SpreadsheetApp.openById("1LlRo5Ob5Bw8penUEakQj_1NyfmmD-T_Dama-k81dohQ").getSheetByName("Sheet1").getRange('A3:AG1000').getValues();
-    dataValues = _filter(dataValues);
+  function _filter(arr) {
+    var the_week = _getToWeek();
+    var _result  = arr.filter(function (val) {
+      if (val[5] != 2) {
+        return false;
+      }
+      var the_week_end   = parseInt(Utilities.formatDate(new Date(the_week.end), 'EST', 'D'));
+      var the_week_start = parseInt(Utilities.formatDate(new Date(the_week.start), 'EST', 'D'));
+      var _theDay        = parseInt(Utilities.formatDate(new Date(val[0]), 'EST', 'D'));
+
+      if (the_week_start > the_week_end) {
+        return _theDay < the_week_end || _theDay > the_week_start;
+      }
+      return _theDay < the_week_end && _theDay > the_week_start;
+    });
+
+    _result.sort(_equalTime);
+    the_week.end.setDate(the_week.end.getDate() + 2);
+    _result.push([the_week.end]);
+    return _result;
+  }
+
+  function _getToWeek() {
+    var _start, _end;
+    var THE_DAY = 4;
+    var _now    = new Date();
+    var delta   = THE_DAY - _now.getDay();
+    delta       = (delta < 0) ? 6 + delta : delta;
+
+    _end   = new Date(_now.getYear(), _now.getMonth(), _now.getDate() + delta + 30);
+    _start = new Date(_now.getYear(), _now.getMonth(), _now.getDate() + delta - 7);
+    return { start: _start, end: _end };
+  }
+
+  /*functions*/
+  function _printTableTitle() {
+    _printDateByLang(2, 'לוח אירועים שבועי', '#6d9eeb').setFontSize(24).setFontWeight('bold');
+    _printDateByLang(6, 'Weekly Events Board', '#00ff00').setFontSize(24).setFontWeight('bold');
+    _printDateByLang(10, 'Расписание на неделю', '#ffff00').setFontSize(24).setFontWeight('bold');
+    _printDateByLang(14, 'Lista de Eventos Semanales', '#e06666').setFontSize(24).setFontWeight('bold');
+    resultSheet.setRowHeight(_resultCurrentRowId, 50);
+  }
+
+  function _printDay(day) {
+    _printDate(day.date);
+    day.events.forEach(function (e) {
+      if (!e.start && !e.end) {
+        return;
+      }
+      _resultCurrentRowId++;
+      resultSheet.getRange(_resultCurrentRowId, 1, 1, 50).clear();
+      resultSheet.setRowHeight(_resultCurrentRowId, 24);
+
+      _ptintHeb(e);
+      _ptintEng(e);
+      _ptintRus(e);
+      _ptintEsp(e);
+      _separator(resultSheet.getRange(_resultCurrentRowId, 17, 1, 1));
+    });
+  }
+
+  function _printDate(data) {
+    _resultCurrentRowId++;
+    resultSheet.getRange(_resultCurrentRowId, 1, 1, 50).clear();
+    resultSheet.setRowHeight(_resultCurrentRowId, 28);
+    var dateStr = Utilities.formatDate(data.date, SpreadsheetApp.getActive().getSpreadsheetTimeZone(), 'dd/MM');
+    //heb
+    _printDateByLang(2, _weekDays[data.weekDay][0] + ' ' + dateStr, '#cfe2f3');
+    //english
+    _printDateByLang(6, _weekDays[data.weekDay][1] + ' ' + dateStr, '#b6d7a8');
+    //rus
+    _printDateByLang(10, _weekDays[data.weekDay][2] + ' ' + dateStr, '#ffe599');
+    //esp
+    _printDateByLang(14, _weekDays[data.weekDay][3] + ' ' + dateStr, '#f4cccc');
+    _separator(resultSheet.getRange(_resultCurrentRowId, 17, 1, 1));
+  }
+
+  function _printDateByLang(colStartNum, textLang, bgColor) {
+    var range = resultSheet.getRange(_resultCurrentRowId, colStartNum, 1, 1);
+    range.setBackground(bgColor);
+    range.setFontSize(16);
+    range.setFontWeight('bold');
+    range.setHorizontalAlignment('center');
+    range.setValue(textLang);
+
+    resultSheet.getRange(_resultCurrentRowId, colStartNum, 1, 3).merge();
+
+    _separator(resultSheet.getRange(_resultCurrentRowId, colStartNum - 1, 1, 1));
+    return range;
+  }
+
+  function _ptintHeb(e) {
+    var timeStr = e.start + '-' + e.end;
+    var textStr = e.heb;
+    _printEvent(timeStr, textStr, 2, true);
+  }
+
+  function _ptintEng(e) {
+    var timeStr = e.start + '-' + e.end;
+    var textStr = e.eng;
+    _printEvent(timeStr, textStr, 6);
+  }
+
+  function _ptintRus(e) {
+    var timeStr = e.start + '-' + e.end;
+    var textStr = e.rus;
+    _printEvent(timeStr, textStr, 10);
+  }
+
+  function _ptintEsp(e) {
+    var timeStr = e.start + '-' + e.end;
+    var textStr = e.esp;
+    _printEvent(timeStr, textStr, 14);
+  }
+
+  function _printEvent(timeStr, textStr, colStartNum, isHeb) {
+    var textColNum = colStartNum;
+    var timeColNum = colStartNum + 1;
+    var timeDir    = 'left';
+
+    if (isHeb) {
+      timeDir    = 'right';
+      textColNum = colStartNum + 2;
+      timeColNum = colStartNum;
+    }
+
+    var rangeText = resultSheet.getRange(_resultCurrentRowId, textColNum);
+    var rangeTime = resultSheet.getRange(_resultCurrentRowId, timeColNum, 1, 2);
+    rangeTime.setHorizontalAlignment(timeDir);
+
+    rangeTime.setValue(timeStr).merge();
+    rangeTime.setFontWeight('bold');
+    rangeTime.setFontSize(12);
+
+    rangeText.setValue(textStr);
+    rangeText.setFontSize(12);
+    rangeText.setFontWeight('normal');
+
+    _separator(resultSheet.getRange(_resultCurrentRowId, colStartNum - 1, 1, 1));
+  }
+
+  function _separator(range) {
+    range.clear().setBackground('#cccccc');
+  }
+
+  function _parseDate(row) {
     return {
-        run: function (runAfterAll) {
-            _separator(resultSheet.getRange(_resultCurrentRowId, 1, 1, 17));
-            _printTableTitle()
-            dataValues.forEach(function (row, key) {
-                if (!!_theDay.date && _theDay.date.date.getTime() != row[2].getTime()) {
-                    _printDay(_theDay);
-                    _theDay = {};
-                }
-                if (!_theDay.date) {
-                    _theDay.date = _parseDate(row);
-                    _theDay.events = [];
-                }
-                _theDay.events.push(_parseEvent(row));
-            });
-            _separator(resultSheet.getRange(_resultCurrentRowId + 1, 1, 1, 17));
-            runAfterAll(_resultCurrentRowId);
-        }
+      weekDay: row[0].getDay(),
+      date: row[0],
+      heb: row[3],
+      eng: row[8],
+      rus: row[11],
+      esp: row[14]
+    };
+  }
+
+  function _parseEvent(row) {
+    return {
+      heb: row[3],
+      eng: row[8],
+      rus: row[11],
+      esp: row[14],
+      start: row[1],
+      end: row[2]
+    };
+  }
+
+  function _equalTime(a, b) {
+    var a2Num = new Number(a[0]);
+    var b2Num = new Number(b[0]);
+    var a5Num = new Number(a[1].split(':')[0]);
+    var b5Num = new Number(b[1].split(':')[0]);
+    if (a2Num < b2Num) {
+      return -1;
     }
-
-    function _filter(arr) {
-        var the_week = _getToWeek();
-        var _result = [];
-
-        _result = arr.filter(function (val, key) {
-            if (val[8] !== 2) {
-                return false;
-            }
-            var the_week_end = parseInt(Utilities.formatDate(new Date(the_week.end), "EST", "D"));
-            var the_week_start = parseInt(Utilities.formatDate(new Date(the_week.start), "EST", "D"));
-            var _theDay = parseInt(Utilities.formatDate(new Date(val[2]), "EST", "D"));
-
-            if (_theDay > the_week_end || _theDay < the_week_start) {
-                return false;
-            }
-
-            return true;
-        });
-
-        _result.sort(function (a, b) {
-            if (a[2] < b[2]) {
-                return -1;
-            }
-            if (a[2] > b[2]) {
-                return 1;
-            }
-            if (a[5] < b[5]) {
-                return -1;
-            }
-            if (a[5] > b[5]) {
-                return 1;
-            }
-            return 0;
-        });
-        the_week.end.setDate(the_week.end.getDate() + 2);
-        _result.push([null, null, the_week.end]);
-        return _result;
+    if (a2Num > b2Num) {
+      return 1;
     }
-
-    function _getToWeek() {
-        var _start, _end;
-        var THE_DAY = 4;
-        var _now = new Date();
-        var delta = THE_DAY - _now.getDay();
-        delta = (delta < 0) ? 6 + delta : delta;
-
-        _end = new Date(_now.getYear(), _now.getMonth(), _now.getDate() + delta + 20);
-        _start = new Date(_now.getYear(), _now.getMonth(), _now.getDate() + delta - 7);
-        return {start: _start, end: _end};
+    if (a5Num < a5Num) {
+      return -1;
     }
-
-
-    /*functions*/
-    function _printTableTitle() {
-        _printDateByLang(2, "לוח אירועים שבועי", "#6d9eeb").setFontSize(24).setFontWeight("bold");
-        _printDateByLang(6, "Weekly Events Board", "#00ff00").setFontSize(24).setFontWeight("bold");
-        _printDateByLang(10, "Расписание на неделю", "#ffff00").setFontSize(24).setFontWeight("bold");
-        _printDateByLang(14, "Lista de Eventos Semanales", "#e06666").setFontSize(24).setFontWeight("bold");
-        resultSheet.setRowHeight(_resultCurrentRowId, 50);
+    if (a5Num > a5Num) {
+      return 1;
     }
-
-    function _printDay(day) {
-        _printDate(day.date);
-        day.events.forEach(function (e) {
-            if (!e.start && !e.end) {
-                return;
-            }
-            _resultCurrentRowId++;
-            resultSheet.getRange(_resultCurrentRowId, 1, 1, 50).clear();
-            resultSheet.setRowHeight(_resultCurrentRowId, 24);
-
-            _ptintHeb(e);
-            _ptintEng(e);
-            _ptintRus(e);
-            _ptintEsp(e);
-            _separator(resultSheet.getRange(_resultCurrentRowId, 17, 1, 1));
-        });
-    }
-
-    function _printDate(date) {
-        _resultCurrentRowId++;
-        resultSheet.getRange(_resultCurrentRowId, 1, 1, 50).clear();
-        resultSheet.setRowHeight(_resultCurrentRowId, 28);
-        var dateStr = Utilities.formatDate(date.date, SpreadsheetApp.getActive().getSpreadsheetTimeZone(), "dd/MM");
-
-        _printDateByLang(2, date.heb + " " + dateStr, "#cfe2f3");
-        _printDateByLang(6, date.eng + " " + dateStr, "#b6d7a8");
-        _printDateByLang(10, date.rus + " " + dateStr, "#ffe599");
-        _printDateByLang(14, date.esp + " " + dateStr, "#f4cccc");
-        _separator(resultSheet.getRange(_resultCurrentRowId, 17, 1, 1));
-    }
-
-
-    function _printDateByLang(colStartNum, textLang, bgColor) {
-        var range = resultSheet.getRange(_resultCurrentRowId, colStartNum, 1, 1);
-        range.setBackground(bgColor);
-        range.setFontSize(16);
-        range.setFontWeight("bold");
-        range.setHorizontalAlignment("center");
-        range.setValue(textLang);
-
-        resultSheet.getRange(_resultCurrentRowId, colStartNum, 1, 3).merge();
-
-        _separator(resultSheet.getRange(_resultCurrentRowId, colStartNum - 1, 1, 1));
-        return range;
-    }
-
-
-    function _ptintHeb(e) {
-        var timeStr = e.start + "-" + e.end;
-        var textStr = e.heb;
-        _printEvent(timeStr, textStr, 2, true);
-    }
-
-    function _ptintEng(e) {
-        var timeStr = e.start + "-" + e.end;
-        var textStr = e.eng;
-        _printEvent(timeStr, textStr, 6);
-    }
-
-    function _ptintRus(e) {
-        var timeStr = e.start + "-" + e.end;
-        var textStr = e.rus;
-        _printEvent(timeStr, textStr, 10)
-    }
-
-    function _ptintEsp(e) {
-        var timeStr = e.start + "-" + e.end;
-        var textStr = e.esp;
-        _printEvent(timeStr, textStr, 14)
-    }
-
-    function _printEvent(timeStr, textStr, colStartNum, isHeb) {
-        var textColNum = colStartNum;
-        var timeColNum = colStartNum + 1;
-        var timeDir = "left";
-
-        if (isHeb) {
-            timeDir = "right"
-            textColNum = colStartNum + 2;
-            timeColNum = colStartNum;
-        }
-
-        var rangeText = resultSheet.getRange(_resultCurrentRowId, textColNum);
-        var rangeTime = resultSheet.getRange(_resultCurrentRowId, timeColNum, 1, 2);
-        rangeTime.setHorizontalAlignment(timeDir);
-
-        rangeTime.setValue(timeStr).merge();
-        rangeTime.setFontWeight("bold");
-        rangeTime.setFontSize(12);
-
-        rangeText.setValue(textStr);
-        rangeText.setFontSize(12);
-        rangeText.setFontWeight("normal");
-
-        _separator(resultSheet.getRange(_resultCurrentRowId, colStartNum - 1, 1, 1));
-    }
-
-    function _separator(range) {
-        range.clear().setBackground("#cccccc");
-    }
-
-
-    function _parseDate(row) {
-        return {
-            date: row[2],
-            heb: row[3],
-            eng: row[14],
-            rus: row[22],
-            esp: row[30]
-        };
-    }
-
-    function _parseEvent(row) {
-        return {
-            heb: row[4],
-            eng: row[13],
-            rus: row[21],
-            esp: row[29],
-            start: row[5],
-            end: row[6]
-        };
-    }
+    return 0;
+  }
 }
